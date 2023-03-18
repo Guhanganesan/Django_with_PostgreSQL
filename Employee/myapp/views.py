44c8af9a7b28305ba50fd10ca1b3934568626682
Guhan Ganesan <phpguhan@gmail.com>
2023-03-19 00:55:52 +0530

    Insert details

--- Employee/myapp/views.py
+++ Employee/myapp/views.py
@@ -3,3 +3,19 @@ from .forms import EmployeeForm
 from .models import Employee
 
 # Create your views here.
+
+# Create Employee
+
+def insert_emp(request):
+    if request.method == "POST":
+        EmpId = request.POST['EmpId']
+        EmpName = request.POST['EmpName']
+        EmpGender = request.POST['EmpGender']
+        EmpEmail = request.POST['EmpEmail']
+        EmpDesignation = request.POST['EmpDesignation']
+        data = Employee(EmpId=EmpId, EmpName=EmpName, EmpGender=EmpGender, EmpEmail=EmpEmail, EmpDesignation= EmpDesignation)
+        data.save()
+  
+        return redirect('show/')
+    else:
+        return render(request, 'insert.html')
