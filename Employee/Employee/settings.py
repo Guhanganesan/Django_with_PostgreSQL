f411099839463a252f5eaeb0ff767c7b311f457f
Guhan Ganesan <phpguhan@gmail.com>
2023-03-19 00:47:27 +0530

    Register Database config and create new urls in myapp

--- Employee/Employee/settings.py
+++ Employee/Employee/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Employee.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,16 @@ WSGI_APPLICATION = 'Employee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'Employee',  
+        'USER':'postgres',  
+        'PASSWORD':'1234',  
+        'HOST':'localhost',  
+        'PORT':'5432'  
+
     }
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Django_with_PostgreSQL
 1. Create Project: django-admin startproject Employee
 2. cd Employee => Create App using 'python manage.py startapp myapp'
+3. Register your app in settings.py file named 'myapp'
+4. Add Templates directory in the settings.py file
+5. Create a PostgreSQL database called Employee and set it up in the Django project’s settings.py file
+6. Create urls.py and views.py in myapp
